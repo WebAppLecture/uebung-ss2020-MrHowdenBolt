3fbddb026c6bcff54a267bf40331bdc6963f5d27
MrHowdenBolt <sebastien.alipour@gmail.com>
2020-04-07 18:14:19 +0200

    commmit on bearbeitung2

--- 02-Calculator/Calculator.js
+++ 02-Calculator/Calculator.js
@@ -3,14 +3,25 @@ import {MyMath} from "../01-MyMath/MyMath.js";
 export class Calculator {
 
     constructor(numpad, outputCalculation, outputSolution) {
+        this.numpad = numpad;
+        this.clearoutputCalculation = outputCalculation;
+        this.outputSolution = outputSolution;
+        this.setupNumPad();
+
     }
 
-    setupNumPad() { 
+    setupNumPad() {
+        let button = this.numpad.children;
+        let length = button.length;
+        for (let i = 0; i < length; i++) {
+            button[i].addEventListener('click', this.onButtonClick.bind(button[i]));
+        } 
 
     }
 
     onButtonClick(symbol) {
-    
+        symbol = symbol.target.textContent;
+        console.log(symbol);
     }
 
     print(string) {
